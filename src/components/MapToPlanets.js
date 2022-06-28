0be1470b3d389f4af544759ec9e66ea95d7607fd
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-06-28 13:00:42 -0400

    feature: implementação do filtro numerico, bem como de butão para exclusão do mesmo

--- src/components/MapToPlanets.js
+++ src/components/MapToPlanets.js
@@ -6,14 +6,11 @@ function MapToPlanets() {
   const [nameSearch, setNameSearch] = useState({ filterByName: { name: '' } });
   const [planetsFilter, setPlanetsFilter] = useState([]);
   const [filterEspec, setFilterEspec] = useState([]);
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState(0);
   const [numericFilter, setNumericFilter] = useState({
-    filterByNumericValues: [
-      {
-        column: '',
-        comparison: '',
-        value: '',
-      },
-    ],
+    filterByNumericValues: [],
   });
 
   useEffect(() => {
@@ -21,14 +18,33 @@ function MapToPlanets() {
       const filterName = planets.filter((planet) => (
         planet.name.toLowerCase().includes(nameSearch.filterByName.name)
       ));
-      setPlanetsFilter(filterName);
+      const activFilter = numericFilter.filterByNumericValues
+        .reduce((acumula, filtro) => (
+          acumula.filter((planeta) => {
+            switch (filtro.comparison) {
+            case 'maior que':
+              return planeta[filtro.column] > filtro.value;
+            case 'menor que':
+              return planeta[filtro.column] < filtro.value;
+            case 'igual a':
+              return planeta[filtro.column] === filtro.value;
+            default:
+              return true;
+            }
+          })
+        ), filterName);
+      setPlanetsFilter(activFilter);
     }
     if (espec.length) {
       setFilterEspec(espec);
     }
-  }, [planets, nameSearch, espec]);
+  }, [planets, nameSearch, espec, numericFilter.filterByNumericValues]);
+
   const columnFilter = () => (
-    <select data-testid="column-filter">
+    <select
+      data-testid="column-filter"
+      onChange={ ({ target }) => setColumn(target.value) }
+    >
       {filterEspec.map((op) => (
         <option
           key={ op }
@@ -42,7 +58,10 @@ function MapToPlanets() {
   );
 
   const comparisonFilter = () => (
-    <select data-testid="comparison-filter">
+    <select
+      data-testid="comparison-filter"
+      onChange={ ({ target }) => setComparison(target.value) }
+    >
       {
         equal
         && equal.map((op) => (
@@ -64,14 +83,45 @@ function MapToPlanets() {
       data-testid="value-filter"
       placeholder="0"
       name="value"
+      onChange={ ({ target }) => setValue(target.value) }
     />
   );
 
+  const handleButton = () => {
+    const obj = {
+      column,
+      comparison,
+      value,
+    };
+    setNumericFilter(
+      {
+        filterByNumericValues: [
+          ...numericFilter.filterByNumericValues,
+          obj,
+        ],
+      },
+    );
+  };
+
   const buttonFilter = () => (
-    <button type="button" data-testid="button-filter">
+    <button
+      type="button"
+      data-testid="button-filter"
+      onClick={ handleButton }
+    >
       Number Filter
     </button>
   );
+
+  const handleDeleteFilter = (i) => {
+    const deleteFilter = numericFilter.filterByNumericValues
+      .filter((_filt, itemIndex) => (
+        itemIndex !== i
+      ));
+    setNumericFilter({
+      filterByNumericValues: [...deleteFilter],
+    });
+  };
 
   return (
     <div>
@@ -99,6 +149,23 @@ function MapToPlanets() {
           <div>segundo forms</div>
         </div>
       </header>
+      <div>
+        {numericFilter
+        && numericFilter.filterByNumericValues.map((filt, i) => (
+          <div key={ ` ${filt.comparison}-${i}` }>
+            <p>
+              {`${filt.column}-${filt.comparison}-${filt.value}`}
+            </p>
+            <button
+              type="button"
+              onClick={ () => handleDeleteFilter(i) }
+            >
+              X
+
+            </button>
+          </div>
+        ))}
+      </div>
       <table>
         <thead>
           <tr>
